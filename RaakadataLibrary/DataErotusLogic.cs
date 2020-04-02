f650d78596d1b388a6bb2d4fb1889e820a21c390
Tero Kilpeläinen <degenone1@gmail.com>
2020-04-02 22:33:28 +0300

    virheilmoitusia lisätty

--- RaakadataLibrary/DataErotusLogic.cs
+++ RaakadataLibrary/DataErotusLogic.cs
@@ -15,8 +15,17 @@ namespace RaakadataLibrary
         /// <returns>Palauttaa raakadatan string muodossa.</returns>
         public static string RaakaDatanErotus(this string raakaTeksti)
         {
+            if (String.IsNullOrEmpty(raakaTeksti))
+            {
+                throw new NullReferenceException("Raakateksti oli tyhjä.");
+            }
             string xmlLoppu = "</CalibrationData>\r\n";
-            raakaTeksti = raakaTeksti.Substring(raakaTeksti.IndexOf(xmlLoppu) + xmlLoppu.Length);
+            int xmlLoppuIdx = raakaTeksti.IndexOf(xmlLoppu);
+            if (xmlLoppuIdx < 0)
+            {
+                throw new ArgumentOutOfRangeException("Raakatekstistä ei löydy xml osaa.");
+            }
+            raakaTeksti = raakaTeksti.Substring(xmlLoppuIdx + xmlLoppu.Length);
             raakaTeksti = raakaTeksti.Substring(raakaTeksti.IndexOf("\r\n") + 2);
             return raakaTeksti;
         }
@@ -29,9 +38,18 @@ namespace RaakadataLibrary
         /// <returns>Palauttaa raakadatan string muodossa.</returns>
         public static string RaakaDatanErotus(this string raakaTeksti, bool headerinPoisto)
         {
+            if (String.IsNullOrEmpty(raakaTeksti))
+            {
+                throw new NullReferenceException("Raakateksti oli tyhjä.");
+            }
             string xmlLoppu = "</CalibrationData>\r\n";
+            int xmlLoppuIdx = raakaTeksti.IndexOf(xmlLoppu);
+            if (xmlLoppuIdx < 0)
+            {
+                throw new ArgumentOutOfRangeException("Raakatekstistä ei löydy xml osaa.");
+            }
             // raakadata headearin kanssa
-            raakaTeksti = raakaTeksti.Substring(raakaTeksti.IndexOf(xmlLoppu) + xmlLoppu.Length);
+            raakaTeksti = raakaTeksti.Substring(xmlLoppuIdx + xmlLoppu.Length);
             // jos headeria ei haluta mukaan dataan
             if (headerinPoisto)
             {
