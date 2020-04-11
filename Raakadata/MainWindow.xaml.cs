59867a947ebd1d0afd582bab4ad8020e3e085488
Tero Kilpeläinen <degenone1@gmail.com>
2020-04-11 18:06:04 +0300

    vt #2

--- Raakadata/MainWindow.xaml.cs
+++ Raakadata/MainWindow.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Ookii.Dialogs.Wpf;
 
 namespace Raakadata
 {
@@ -34,5 +35,105 @@ namespace Raakadata
 
         private void ListaaTiedostot() =>
             tbValitutTiedostot.Text = String.Join("\r\n", SeamodeReader.HaeTiedostotListaan(tbTiedostoPolku.Text));
+
+        private void BtnHaeTiedostoPolku_Click(object sender, RoutedEventArgs e)
+        {
+            // https://stackoverflow.com/questions/1922204/open-directory-dialog
+            // Ookii.Diologs.Wpf
+            var fd = new VistaFolderBrowserDialog();
+            if (fd.ShowDialog(this).GetValueOrDefault())
+            {
+                tbTiedostoPolku.Text = fd.SelectedPath;
+                ListaaTiedostot();
+            }
+        }
+
+        private void BtnHaeTallennusPolku_Click(object sender, RoutedEventArgs e)
+        {
+            var fd = new VistaFolderBrowserDialog();
+            if (fd.ShowDialog(this).GetValueOrDefault())
+            {
+                tbTallennusPolku.Text = fd.SelectedPath;
+            }
+        }
+
+        private void BtnLuoKisaTiedosto_Click(object sender, RoutedEventArgs e)
+        {
+            if (dpAlkuPvm.SelectedDate == null || dpLoppuPvm.SelectedDate == null)
+            {
+                MessageBox.Show("Please select start and end dates for the race.");
+                return;
+            }
+            DateTime alku = (DateTime)dpAlkuPvm.SelectedDate;
+            DateTime loppu = (DateTime)dpLoppuPvm.SelectedDate;
+            if (loppu <= alku)
+            {
+                MessageBox.Show("Check the dates and times.\nRace start must be before the ending.");
+                return;
+            }
+            BtnLuoKisaTiedosto.IsEnabled = false;
+            SeamodeReader sr = new SeamodeReader(alku, loppu);
+            foreach (string tiedosto in sr.HaeTiedostot(tbTiedostoPolku.Text))
+            {
+                sr.LueTiedosto(tiedosto);
+            }
+            SeamodeWriter sw = new SeamodeWriter() { OutFile = tbKilpaTiedostoPolku.Text };
+            sw.Kirjoita(sr.Rivit);
+            MessageBox.Show($"File {tbKilpaTiedostoPolku.Text} was created.");
+            BtnLuoKisaTiedosto.IsEnabled = true;
+        }
+
+        private void TbKilpailuNimi_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string aika = textBoxStart == null ? "" : String.Join("", textBoxStart.Text.Split(':'));
+            tbKilpaTiedostoPolku.Text = $"{tbTallennusPolku.Text}\\SeaMODE_{dpAlkuPvm.DisplayDate:yyyyMMdd}_{aika}_{tbKilpailuNimi.Text}.csv";
+        }
+
+        private void TbTallennusPolku_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string nimi = tbKilpailuNimi == null ? "" : tbKilpailuNimi.Text;
+            if (tbKilpaTiedostoPolku != null)
+            {
+                string aika = textBoxStart == null ? "" : String.Join("", textBoxStart.Text.Split(':'));
+                tbKilpaTiedostoPolku.Text = $"{tbTallennusPolku.Text}\\SeaMODE_{dpAlkuPvm.DisplayDate:yyyyMMdd}_{aika}_{nimi}.csv";
+            }
+        }
+
+        private void TbAika_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox tb = e.Source as TextBox;
+            if (tb.Text == "HH.mm:ss" || String.IsNullOrEmpty(tb.Text))
+            {
+                return;
+            }
+            int pituus = tb.Text.Length;
+            if ((pituus != 3 && pituus != 6) && !char.IsDigit(tb.Text, pituus - 1))
+            {
+                tb.Text = tb.Text.Substring(0, pituus - 1);
+            }
+            if (pituus == 2)
+            {
+                tb.Text += ":";
+            }
+            else if (pituus == 5)
+            {
+                tb.Text += ":";
+            }
+            tb.SelectionStart = tb.Text.Length;
+        }
+
+        private void TbAika_GotFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox tb = e.Source as TextBox;
+            if (tb.Text == "HH.mm:ss")
+            {
+                tb.Clear(); 
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
