179d1ec2a9bbf6ce8c55a928e8babddb156a3f03
Tero Kilpeläinen <degenone1@gmail.com>
2020-04-26 21:05:27 +0300

    vt1

--- RaakadataLibrary/SeamodeReader.cs
+++ RaakadataLibrary/SeamodeReader.cs
@@ -16,8 +16,9 @@ namespace RaakadataLibrary
             endTime = lopetusAika;
             string pvmFormator = string.Format("yyyyMMdd");
             startPattern = startPattern_c + startTime.ToString(pvmFormator);
+            endPattern = startPattern_c + endTime.ToString(pvmFormator);
             Rivit = new ArrayList();
-
+            ReaderErrors = new List<string>();
         }
 
         // ei käytetä ollenkaan?
@@ -27,12 +28,17 @@ namespace RaakadataLibrary
         private readonly DateTime endTime;
         private const string startPattern_c = "^SeaMODE_";
         private readonly string startPattern;
+        private readonly string endPattern;
         private readonly CultureInfo cultureInfo = new CultureInfo("fi-FI");
-
+        private int columnCount;
+        // liian suuri ajanmuutos = virhe, mutta missä on raja?
+        private TimeSpan maximumTimeStep = TimeSpan.FromMilliseconds(250);
+        private DateTime previousTime;
 
         public ArrayList Rivit { get; }
         public string OutDire { get; set; }
-        
+        public int DataRowCount { get; private set; } = 0;
+        public List<string> ReaderErrors { get; private set; }
         // tämä on vain tiedostojen listausta wpf:ää varten.
         public static List<string> HaeTiedostotListaan(string polku)
         {
@@ -58,7 +64,7 @@ namespace RaakadataLibrary
             int pit = esimerkki.Length;
             foreach (var fi in di.GetFiles())
             {
-                if (Regex.IsMatch(fi.Name, startPattern) && Regex.IsMatch(fi.Name, ".csv$") && fi.Name.Length == pit)
+                if ((Regex.IsMatch(fi.Name, startPattern) || Regex.IsMatch(fi.Name, endPattern)) && Regex.IsMatch(fi.Name, ".csv$") && fi.Name.Length == pit)
                     filekset.Add(fi.FullName);
                 if (OutDire == null)
                     OutDire = new string(fi.DirectoryName.ToCharArray());
@@ -69,7 +75,8 @@ namespace RaakadataLibrary
         // Haetaan rivit yhdelle tiedostolle
         public void LueTiedosto(string fileName)
         {
-            string riviOtsikkoPattern = "^Date_PC;Time_PC";
+            string riviOtsikkoPattern = "^Date_PC(.*)Time_PC";
+            string[] seperator = { ";" };
             bool isOtsikkoOhi = false;
             using (StreamReader sr = File.OpenText(fileName))
             {
@@ -79,37 +86,63 @@ namespace RaakadataLibrary
                     if (isOtsikkoOhi)
                     {
                         // Tarkistetaan sopiiko aika -> string splitillä haetaan aika
-                        if (TarkistaAika(luettu))
-                            Rivit.Add(luettu);
+                        string[] rowValues = luettu.Split(seperator, StringSplitOptions.RemoveEmptyEntries);
+                        if (TarkistaAika(rowValues))
+                        {
+                            if (rowValues.Length == columnCount)
+                            {
+                                DataRowCount++;
+                                Rivit.Add(luettu);
+                            }
+                            else
+                                ReaderErrors.Add($"There was missing data on row {Rivit.Count}. The row was disregarded.");
+                        }
                     }
                     // Otsikkotiedot vain kerran
                     if (!IsOtsikkoTehty && !isOtsikkoOhi)
                     {
                         Rivit.Add(luettu);
-                        if (Regex.IsMatch(luettu, riviOtsikkoPattern))
+                        Match headerMatch = Regex.Match(luettu, riviOtsikkoPattern);
+                        if (headerMatch.Success)
                         {
+                            columnCount = luettu.Split(seperator, StringSplitOptions.RemoveEmptyEntries).Length;
                             IsOtsikkoTehty = true;
                             isOtsikkoOhi = true;
+                            seperator[0] = headerMatch.Groups[1].ToString();
                         }
 
                     }
                     // Otsikko luettu myös ensimmäisen tiedoston jälkeen.
-                    if (IsOtsikkoTehty && !isOtsikkoOhi && Regex.IsMatch(luettu, riviOtsikkoPattern))
-                        isOtsikkoOhi = true;
+                    if (IsOtsikkoTehty && !isOtsikkoOhi)
+                    {
+                        Match headerMatch = Regex.Match(luettu, riviOtsikkoPattern);
+                        if (headerMatch.Success)
+                        {
+                            seperator[0] = headerMatch.Groups[1].ToString();
+                            isOtsikkoOhi = true;
+                        }
+                    }
                 }
             }
         }
         // Tarkistetaan aika
-        private bool TarkistaAika(string luettuRivi)
+        private bool TarkistaAika(string[] arvot)
         {
-            string[] arvot = luettuRivi.Split(';');
-            // Rivissä jotain vikaa
-            if (arvot.Length < 2)
-                return false;
             // ensimmäisessä alkiossa pvm muodossa pp.kk.vvvv ja toisessa aika hh:mm:ss.nnn
             DateTime tapahtumaAika = DateTime.ParseExact(arvot[0] + " " + arvot[1], "dd.MM.yyyy HH:mm:ss.fff", cultureInfo);
             if (tapahtumaAika >= startTime && tapahtumaAika <= endTime)
+            {
+                if (previousTime == null)
+                    previousTime = tapahtumaAika;
+                else if (tapahtumaAika - previousTime > maximumTimeStep)
+                {
+                    ReaderErrors.Add($"There was a large time difference at row {Rivit.Count}. The row was disregarded.");
+                    previousTime = tapahtumaAika;
+                    return false;
+                }
+                previousTime = tapahtumaAika;
                 return true;
+            }
             else
                 return false;
         }
