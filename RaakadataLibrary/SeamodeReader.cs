29fbe691796a38d6a939c9e7c11a7635458f4b50
Tero Kilpeläinen <48437506+degenone@users.noreply.github.com>
2020-05-08 19:39:15 +0300

    Tiedoston kirjoitus muutos ja koodin käännös (#8)

--- RaakadataLibrary/SeamodeReader.cs
+++ RaakadataLibrary/SeamodeReader.cs
@@ -3,27 +3,30 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
 
 namespace RaakadataLibrary
 {
     public class SeamodeReader
     {
         // Annetaan start date ja time 
-        public SeamodeReader(DateTime aloitusAika, DateTime lopetusAika)
+        public SeamodeReader(DateTime _startTime, DateTime _endTime)
         {
-            startTime = aloitusAika;
-            endTime = lopetusAika;
-            string pvmFormator = string.Format("yyyyMMdd");
-            startPattern = startPattern_c + startTime.ToString(pvmFormator);
-            endPattern = startPattern_c + endTime.ToString(pvmFormator);
-            Rivit = new ArrayList();
-            ReaderErrors = new List<string>();
+            startTime = _startTime;
+            endTime = _endTime;
+            string dateFormat = string.Format("yyyyMMdd");
+            startPattern = startPattern_c + startTime.ToString(dateFormat);
+            endPattern = startPattern_c + endTime.ToString(dateFormat);
+            DataRowErrors = new List<string>();
         }
 
-        private bool IsOtsikkoTehty = false;
+        private bool headersWritten = false;
         private readonly DateTime startTime;
         private readonly DateTime endTime;
+        private bool pastEnd = false;
         private const string startPattern_c = "^SeaMODE_";
         private readonly string startPattern;
         private readonly string endPattern;
@@ -31,101 +34,142 @@ namespace RaakadataLibrary
         private int columnCount;
         // liian suuri ajanmuutos = virhe, mutta missä on raja?
         private TimeSpan maximumTimeStep = TimeSpan.FromSeconds(1);
-        private DateTime previousTime;
+        private DateTime? prevEventTime = null;
+        private readonly string headerRowPattern = "^Date_PC(.*)Time_PC";
 
-        public ArrayList Rivit { get; }
-        public string OutDire { get; set; }
+        public string TmpFile = Path.GetTempFileName();
         public List<GpxLine> gpxLines { get; set; }
         public int DataRowCount { get; private set; } = 0;
-        public List<string> ReaderErrors { get; private set; }
+        public List<string> DataRowErrors { get; private set; }
+        // jotta käyttöliittymä ei lukkiudu tiedoston luku ja kirjoituksen ajaksi
+        public async Task ReadAndWriteFilesAsync(string path) => await Task.Run(() => ForeachFileIn(FetchFilesToRead(path)));
+
+        private void ForeachFileIn(List<string> files)
+        {
+            foreach (string file in files)
+            {
+                ReadAndWriteDataFile(file);
+            }
+        }
+
         // tämä on vain tiedostojen listausta wpf:ää varten.
-        public static List<string> HaeTiedostotListaan(string polku)
+        public static List<string> FetchFilesToList(string path)
         {
-            List<string> filekset = new List<string>();
-            DirectoryInfo di = new DirectoryInfo(polku);
-            string esimerkki = "SeaMODE_20190928_112953.csv";
-            int pit = esimerkki.Length;
+            List<string> files = new List<string>();
+            DirectoryInfo di = new DirectoryInfo(path);
+            string example = "SeaMODE_20190928_112953.csv";
+            int len = example.Length;
             foreach (var fi in di.GetFiles())
             {
-                if (Regex.IsMatch(fi.Name, "^SeaMODE_") && Regex.IsMatch(fi.Name, ".csv$") && fi.Name.Length == pit)
-                    filekset.Add(fi.FullName);
+                if (Regex.IsMatch(fi.Name, "^SeaMODE_") && Regex.IsMatch(fi.Name, ".csv$") && fi.Name.Length == len)
+                    files.Add(fi.FullName);
             }
 
-            return filekset;
+            return files;
         }
 
-        public List<string> HaeTiedostot(string polku)
+        public List<string> FetchFilesToRead(string path)
         {
-            List<string> filekset = new List<string>();
             // Muuta seuraavat syötteeksi tai jostain configista haettavaksi
-            DirectoryInfo di = new DirectoryInfo(polku);
-            string esimerkki = "SeaMODE_20190928_112953.csv";
-            int pit = esimerkki.Length;
+            List<string> files = new List<string>();
+            DirectoryInfo di = new DirectoryInfo(path);
+            string example = "SeaMODE_20190928_112953.csv";
+            int len = example.Length;
             foreach (var fi in di.GetFiles())
             {
-                if ((Regex.IsMatch(fi.Name, startPattern) || Regex.IsMatch(fi.Name, endPattern)) && Regex.IsMatch(fi.Name, ".csv$") && fi.Name.Length == pit)
-                    filekset.Add(fi.FullName);
-                if (OutDire == null)
-                    OutDire = new string(fi.DirectoryName.ToCharArray());
+                if (pastEnd)
+                    break;
+                if ((Regex.IsMatch(fi.Name, startPattern) || Regex.IsMatch(fi.Name, endPattern)) && Regex.IsMatch(fi.Name, ".csv$") && fi.Name.Length == len)
+                    files.Add(fi.FullName);
             }
 
-            return filekset;
+            return files;
         }
+
         // Haetaan rivit yhdelle tiedostolle
-        public void LueTiedosto(string fileName)
+        private void ReadAndWriteDataFile(string filePath)
         {
-            string riviOtsikkoPattern = "^Date_PC(.*)Time_PC";
-            string[] seperator = { ";" };
-            bool isOtsikkoOhi = false;
-            using (StreamReader sr = File.OpenText(fileName))
+            string[] separator = { ";" };
+            bool validFile = true;
+            bool headersFound = false;
+            List<string> headerRows = new List<string>();
+            int rowNum = 1;
+            using (StreamWriter sw = File.AppendText(TmpFile))
+            using (StreamReader sr = File.OpenText(filePath))
             {
-                string luettu = "";
-                while ((luettu = sr.ReadLine()) != null)
+                string row = "";
+                while ((row = sr.ReadLine()) != null && validFile && !pastEnd)
                 {
-                    if (isOtsikkoOhi)
+                    if (headersFound)
                     {
                         // Tarkistetaan sopiiko aika -> string splitillä haetaan aika
-                        string[] rowValues = luettu.Split(seperator, StringSplitOptions.RemoveEmptyEntries);
-                        if (TarkistaAika(rowValues))
+                        string[] rowValues = row.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+                        if (TimeValidation(rowValues, rowNum))
                         {
+                            if (!headersWritten)
+                            {
+                                foreach (string line in headerRows)
+                                {
+                                    sw.WriteLine(line);
+                                }
+                                columnCount = headerRows.Last().Split(separator, StringSplitOptions.RemoveEmptyEntries).Length;
+                                headerRows.Clear();
+                                headerRows.TrimExcess();
+                                headersWritten = true;
+                            }
                             if (rowValues.Length == columnCount)
                             {
                                 DataRowCount++;
-                                Rivit.Add(luettu);
+                                sw.WriteLine(row);
                             }
                             else
-                                ReaderErrors.Add($"There was missing data on row {Rivit.Count}. The row was disregarded.");
+                                DataRowErrors.Add($"There was missing data on row {rowNum}. The row was disregarded.");
                         }
                     }
-                    // Otsikkotiedot vain kerran
-                    if (!IsOtsikkoTehty && !isOtsikkoOhi)
-                    {
-                        Rivit.Add(luettu);
-                        Match headerMatch = Regex.Match(luettu, riviOtsikkoPattern);
-                        if (headerMatch.Success)
-                        {
-                            columnCount = luettu.Split(seperator, StringSplitOptions.RemoveEmptyEntries).Length;
-                            IsOtsikkoTehty = true;
-                            isOtsikkoOhi = true;
-                            seperator[0] = headerMatch.Groups[1].ToString();
-                        }
-                    }
-                    // Otsikko luettu myös ensimmäisen tiedoston jälkeen.
-                    if (IsOtsikkoTehty && !isOtsikkoOhi)
-                    {
-                        Match headerMatch = Regex.Match(luettu, riviOtsikkoPattern);
-                        if (headerMatch.Success)
-                        {
-                            seperator[0] = headerMatch.Groups[1].ToString();
-                            isOtsikkoOhi = true;
-                        }
-                    }
+                    else
+                        FileValidation(headerRows, rowNum, row, ref validFile, ref headersFound, ref separator);
+                    rowNum++;
                 }
+                if (!validFile)
+                    DataRowErrors.Add($"There was something wrong with the xml section in file:\n{filePath}");
             }
         }
+
+        private void FileValidation(List<string> headerRows,
+                                    int rowNum,
+                                    string row,
+                                    ref bool validFile,
+                                    ref bool headersFound,
+                                    ref string[] separator)
+        {
+            // testaus näyttääkö tiedosto oikealta, jos ei muistiin kirjoittaminen lopetetaan.
+            switch (rowNum)
+            {
+                case 1:
+                    if (!row.StartsWith("<?xml version="))
+                        validFile = false;
+                    break;
+                case 2:
+                    if (!row.StartsWith("<CalibrationData>"))
+                        validFile = false;
+                    break;
+                default:
+                    Match headerMatch = Regex.Match(row, headerRowPattern);
+                    if (!Regex.IsMatch(row, @"^\s+<") && !row.StartsWith("</CalibrationData>") && !headerMatch.Success)
+                        validFile = false;
+                    else if (headerMatch.Success)
+                    {
+                        separator[0] = headerMatch.Groups[1].ToString();
+                        headersFound = true;
+                    }
+                    break;
+            }
+            if (validFile && !headersWritten)
+                headerRows.Add(row);
+        }
+
         public void haeGpxData(string fileName)
         {
-            string riviOtsikkoPattern = "^Date_PC(.*)Time_PC";
             string[] seperator = { ";" };
             bool isOtsikkoOhi = false;
             using (StreamReader sr = File.OpenText(fileName))
@@ -135,7 +179,7 @@ namespace RaakadataLibrary
                 while ((luettu = sr.ReadLine()) != null)
                 {
                     string[] rowValues = luettu.Split(seperator, StringSplitOptions.RemoveEmptyEntries);
-                    if (isOtsikkoOhi && TarkistaAika(rowValues))
+                    if (isOtsikkoOhi && TimeValidation(rowValues))
                     {
                         //Nullable<DateTime> prevDateTime = null;
                         DateTime newDateTime;
@@ -148,14 +192,14 @@ namespace RaakadataLibrary
                             prevDateTime = muodostoGpxAika(luettu);
                         }
                     }
-                    if (Regex.IsMatch(luettu, riviOtsikkoPattern))
+                    if (Regex.IsMatch(luettu, headerRowPattern))
                     {
                         isOtsikkoOhi = true;
                     }
                 }
             }
-
         }
+
         private void TeeGpx(string luettuRivi)
         {
             if (gpxLines == null)
@@ -169,25 +213,39 @@ namespace RaakadataLibrary
             gpxLine.setLongitude(arvot[27]);
             gpxLines.Add(gpxLine);
         }
-        // Tarkistetaan aika
-        private bool TarkistaAika(string[] arvot)
+
+        private bool TimeValidation(string[] values)
         {
             // ensimmäisessä alkiossa pvm muodossa pp.kk.vvvv ja toisessa aika hh:mm:ss.nnn
-            DateTime tapahtumaAika = DateTime.ParseExact(arvot[0] + " " + arvot[1], "dd.MM.yyyy HH:mm:ss.fff", cultureInfo);
-            if (tapahtumaAika >= startTime && tapahtumaAika <= endTime)
+            DateTime eventTime = DateTime.ParseExact(values[0] + " " + values[1], "dd.MM.yyyy HH:mm:ss.fff", cultureInfo);
+            return (eventTime >= startTime && eventTime <= endTime) ? true : false;
+        }
+
+        // Tarkistetaan aika
+        private bool TimeValidation(string[] values, int rowNum)
+        {
+            // ensimmäisessä alkiossa pvm muodossa pp.kk.vvvv ja toisessa aika hh:mm:ss.nnn
+            DateTime eventTime = DateTime.ParseExact(values[0] + " " + values[1], "dd.MM.yyyy HH:mm:ss.fff", cultureInfo);
+            if (eventTime > endTime)
             {
-                if (previousTime != null && tapahtumaAika - previousTime > maximumTimeStep)
+                pastEnd = true;
+                return false;
+            }
+            else if (eventTime >= startTime && eventTime <= endTime)
+            {
+                if (prevEventTime != null && eventTime - prevEventTime > maximumTimeStep)
                 {
-                    ReaderErrors.Add($"There was a large time difference at row {Rivit.Count}. The row was disregarded.");
-                    previousTime = tapahtumaAika;
+                    DataRowErrors.Add($"There was a large time difference between row {rowNum} and the previous row.");
+                    prevEventTime = eventTime;
                     return false;
                 }
-                previousTime = tapahtumaAika;
+                prevEventTime = eventTime;
                 return true;
             }
             else
                 return false;
         }
+
         private DateTime muodostoGpxAika(string luettuRivi)
         {
             string[] arvot = luettuRivi.Split(';');
