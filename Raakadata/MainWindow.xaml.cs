9e84dc56f2b442deef058432dcb79fe21b9b6e7e
Jyrki Reijonen <48332704+reijojy@users.noreply.github.com>
2020-06-07 18:29:45 +0300

    GPX tiedoston nimen korjaus

--- Raakadata/MainWindow.xaml.cs
+++ Raakadata/MainWindow.xaml.cs
@@ -1007,7 +1007,7 @@ namespace Raakadata
         private string  getGpxFileName()
         {
             string fullFileName;
-            if(Regex.IsMatch(tbEventName.Text, ".gpx$") || Regex.IsMatch(tbEventName.Text, ".GPX$"))
+            if(Regex.IsMatch(tbEventName.Text, "\\.gpx$") || Regex.IsMatch(tbEventName.Text, "\\.GPX$"))
                 fullFileName = tbSavePath.Text + @"/" + tbEventName.Text;
             else
                 fullFileName = tbSavePath.Text + @"/" + tbEventName.Text + ".gpx";
